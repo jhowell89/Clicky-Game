97c04cb35b85f06a69f013135cf55954d1da1914
jhowell89 <53121363+jhowell89@users.noreply.github.com>
2020-02-11 11:31:38 -0500

    homework

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => <div className="footer text-center">Jarez Howell</div>;
+
+export default Footer;
---
+++ src/images.js
@@ -0,0 +1,19 @@
+import bark from './images/bark.png';
+import donna from './images/donna.png';
+import hannah from './images/hannah.png';
+import jim from './images/jim.png';
+import sarah from './images/sarah.png';
+import puffy from './images/puffy.png';
+
+
+const images = [
+  bark,
+  donna,
+  hannah,
+  jim,
+  sarah,
+  puffy,
+  
+];
+
+export default images;
